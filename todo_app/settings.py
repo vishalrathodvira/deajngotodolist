566156c0f22aad9553eaf99ba83276987fec3d39
vishal rathod <vishalrathodjob0002@gmail.com>
2022-06-06 15:26:41 +0530

    first1 commit

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -126,4 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STTIC_ROOT =  os . path . join(BASE_DIR,'staticfiles')
