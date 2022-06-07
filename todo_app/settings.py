189de992c07dbbe58fffe85c3b82df86d4c5ae98
vishal rathod <vishalrathodjob0002@gmail.com>
2022-06-07 10:16:49 +0530

    firs21 commit

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-l80arx#auha@m#-&9kuj#b$61yeh2ly!gnr$8oy(43!s2wfq2^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["django-todo-list123.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["*","sahajach12345.herokuapp.com","localhost"]
 
 
 # Application definition
